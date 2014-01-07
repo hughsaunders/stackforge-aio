a9730b7f23e9d15087801399991abaeb4f7b8773
Hugh Saunders <hugh@wherenow.org>
2014-01-07 10:20:39 +0000

    fix sudo username issue

--- lib.sh
+++ lib.sh
@@ -8,11 +8,13 @@ git_clone(){
 }
 
 setup_ssh(){
-    mkdir -p ~/.ssh
-    [ -f ~/.ssh/id_rsa ] || ssh-keygen -N '' -f ~/.ssh/id_rsa
-    cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+    # under sudo, ~ expands to non-sudo user dir
+    user=${1:-root}
+    mkdir -p /home/$user/.ssh
+    [ -f /home/$user/.ssh/id_rsa ] || ssh-keygen -N '' -f ~/.ssh/id_rsa
+    cat /home/$user/.ssh/id_rsa.pub >> /home/$user/.ssh/authorized_keys
     echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
-    ssh localhost date
+    ssh $user@localhost date
 }
 
 chef_zero(){
