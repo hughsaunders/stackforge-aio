4dcf2c21d9fa5e31419e9495895b594ef983a0e0
Hugh Saunders <hugh@wherenow.org>
2014-01-15 13:37:06 +0000

    define env based on nova attribs, not sample env :(

--- lib.sh
+++ lib.sh
@@ -107,38 +107,28 @@ override_attributes(
     "compute" => {
         "libvirt" => {
             "virt_type" => "qemu"
-        }
+        },
+        "networks" => [
+            {
+                "label" => "public",
+                "ipv4_cidr" => "192.168.100.0/24",
+                "num_networks" => "1",
+                "network_size" => "255",
+                "bridge" => "br100",
+                "bridge_dev" => "eth1",
+                "dns1" => "8.8.8.8",
+                "dns2" => "8.8.4.4",
+                "multi_host" => "T"
+            }
+        ]
     }
   },
    "glance" => {
      "images" => ["precise","cirros"],
      "image" => {
-       "cirros" => "http://hypnotoad/cirros-0.3.0-x86_64-disk.img",
-       "precise" => "http://hypnotoad/precise-server-cloudimg-amd64.tar.gz"
+       "cirros" => "https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img",
+       "precise" => "http://cloud-images.ubuntu.com/precise/current/precise-server-cloudimg-amd64-disk1.img"
      }
-   },
-  "osops_networks" => {
-     "public" => "10.0.111.0/24",
-     "management" => "10.0.111.0/24",
-     "nova" => "10.0.111.0/24"
-   },
-   "nova" => {
-     "network" => {
-       "fixed_range" => "192.168.100.0/24",
-       "public_interface" => "eth0"
-     },
-     "networks" => [
-       {
-         "label" => "public",
-         "ipv4_cidr" => "192.168.100.0/24",
-         "num_networks" => "1",
-         "network_size" => "255",
-         "bridge" => "br100",
-         "bridge_dev" => "eth1",
-         "dns1" => "8.8.8.8",
-         "dns2" => "8.8.4.4"
-       }
-     ]
    }
   )
 EOF
